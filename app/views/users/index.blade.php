9fc088541658111d5a799852a0b1b4ffe88f38b1
Popa Bogdan Constantin <bogdan.popa@reea.net>
2014-11-18 16:05:10 +0200

    blade @yield and @section

--- app/views/users/index.blade.php
+++ app/views/users/index.blade.php
@@ -1,23 +1,22 @@
-<!doctype html>
-<html>
-  <head>
-  </head>
-  <meta charset="utf-8">
-  <body> 
+@extends('layouts.default')
+
+
+@section('content')
     <h1>All Users:</h1>
 
     @if (count($users) >= 1)
-       <p>Here is the list </p>
-       @foreach ($users as $user)
-        <li>{{ link_to("/users/{$user->username}", $user->username)}}</li>
-        <!--<li>{{ $user->username }}</li>-->
-       @endforeach
-      
+
+        <p>Here is the list </p>
+
+        @foreach ($users as $user)
+
+          <li>{{ link_to("/users/{$user->username}", $user->username)}}</li>
+          
+        @endforeach
+
     @else
-    <p> There are no users in this database </p>
+    
+        <p> There are no users in this database </p>
+        
     @endif
-   
-</body>
-
-
-</html>
+@stop
--- app/views/users/show.blade.php
+++ app/views/users/show.blade.php
@@ -1,13 +1,9 @@
-<!doctype html>
-<html>
-  <head>
-  </head>
-  <meta charset="utf-8">
-  <body> 
+@extends('layouts/default')
+
+
+@section('content')
+
     <h1>Hello, {{ $user->username }} </h1>
     <h2> Your passwd is : {{$user->password}} </h2>
- 
-</body>
-
-
-</html>
+    
+@stop
